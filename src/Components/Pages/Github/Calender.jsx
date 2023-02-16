ec64f8cf0ec160f5fe03ecea8ddb5b178ac1dea6
Gautam Sonkar <gautamsonkar334@gmail.com>
2023-02-16 17:04:15 +0530

    port

--- src/Components/Pages/Github/Calender.jsx
+++ src/Components/Pages/Github/Calender.jsx
@@ -13,8 +13,8 @@ export const Calender = () => {
      <Heading mb="2%" color={"yellow"} bg="#000" borderBottom="10px solid tomato" p="1%" mt={{md:"5%"}}
             borderRightRadius={"50%"}
             borderLeftRadius="50%">Days I code</Heading>
-     <Box  w={["90%","90%","60%","60%"]} m="auto" fontSize={{sm:".5rem"}}>
-     <GitHubCalendar username="gautam2s0"   />
+     <Box  w={["90%","90%","60%","60%"]} m="auto" fontSize={{sm:".5rem"}} >
+     <GitHubCalendar username="gautam2s0" theme={"#000"}    />
      </Box>
    </Stack>
   )
